573deba7070d81db9ecab67e441470eb311f8479
Viktor Savonin <viktor.savonin@meawallet.com>
2017-08-09 08:59:02 +0300

    Lesson 6 - add User and ShoppingList entities

---
+++ src/main/java/lv/javaguru/java2/domain/ShoppingList.java
@@ -0,0 +1,32 @@
+package lv.javaguru.java2.domain;
+
+public class ShoppingList extends BaseEntity {
+
+    private Long id;
+    private String title;
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/lv/javaguru/java2/domain/ShoppingListBuilder.java
@@ -0,0 +1,43 @@
+package lv.javaguru.java2.domain;
+
+public class ShoppingListBuilder {
+
+    private Long id;
+    private String title;
+    private User user;
+
+    private ShoppingListBuilder() {}
+
+    public static ShoppingListBuilder createShoppingList() {
+        return new ShoppingListBuilder();
+    }
+
+    public ShoppingList build() {
+        ShoppingList list = new ShoppingList();
+        list.setId(id);
+        list.setTitle(title);
+        list.setUser(user);
+        return list;
+    }
+
+    public ShoppingListBuilder withId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public ShoppingListBuilder withTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public ShoppingListBuilder withUser(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public ShoppingListBuilder withUser(UserBuilder userBuilder) {
+        this.user = userBuilder.build();
+        return this;
+    }
+
+}
